d211db73bfae038fc98fc8c5edd11c2c15e19cce
k90armstrong <k90armstrong@gmail.com>
2017-09-20 16:02:18 -0600

    adds tasks

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,9 +1,18 @@
-$.ajax({
-    url: 'https://192.168.30.245:2525/api/update/insert',
-    type: 'get',
-    dataType: 'json',
-    success: function (data) {
-        $('h1').html(data.msg);
-    },
-    data: {task: "Change the filter"}
+
+// load the tasks
+// build them
+
+$('button').click(function(){
+    insertTaskAjax({task: $('#task').val()});
 });
+function insertTaskAjax (task) {
+    $.ajax({
+        url: 'http://192.168.30.245:2525/api/update/insert',
+        type: 'get',
+        dataType: 'json',
+        success: function (data) {
+            $('h1').html(data.msg);
+        },
+        data: task
+    });
+}
